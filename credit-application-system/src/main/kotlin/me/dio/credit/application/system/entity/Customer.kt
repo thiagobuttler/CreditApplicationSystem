10ae0e50ef4ab5f5b83c580597b2a5a51a1e8be4
Thiago <thiagobuttler@outlook.com>
2024-02-17 16:43:40 -0300

    Criando as Classes da Camada de Servicos

--- credit-application-system/src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/entity/Customer.kt
@@ -5,7 +5,7 @@ import jakarta.persistence.*
 
 @Entity
 @Table(name = "Customer")
-data class Custumer(
+data class Customer(
     @Column(nullable = false) var firstName: String = "",
     @Column(nullable = false) var lastName: String = "",
     @Column(nullable = false, unique = true) val cpf: String,
@@ -14,7 +14,7 @@ data class Custumer(
     @Column(nullable = false) @Embedded var address: Address = Address(),
     @Column(nullable = false) @OneToMany(fetch = FetchType.LAZY,
         cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
-    mappedBy = "custumer")
+    mappedBy = "customer")
     val credits: List<Credit> = mutableListOf(),
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) val id: Long? = null
 )
