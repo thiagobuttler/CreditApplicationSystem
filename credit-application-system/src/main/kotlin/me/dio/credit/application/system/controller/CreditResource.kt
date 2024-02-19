59aa549bac2c7fc4a26ddb5dd6ce766baa946669
Thiago <thiagobuttler@outlook.com>
2024-02-18 22:26:35 -0300

    Funcao findByCreditCode e classe CreditView

--- credit-application-system/src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
@@ -1,10 +1,13 @@
 package me.dio.credit.application.system.controller
 
 import me.dio.credit.application.system.dto.CreditDto
+import me.dio.credit.application.system.dto.CreditView
 import me.dio.credit.application.system.dto.CreditViewList
 import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.service.impl.CreditService
+import java.util.*
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -23,12 +26,21 @@ class CreditResource(
     fun saveCredit(@RequestBody creditDto: CreditDto): String {
         val credit: Credit = this.creditService.save(creditDto.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+    }
 
-        @GetMapping
-        fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditViewList> {
-            return this.creditService.findAllByCustomer(customerId).stream()
-                .map { credit: Credit -> CreditViewList(credit) }
-                .collect(Collectors.toList())
-        }
+    @GetMapping
+    fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditViewList> {
+        return this.creditService.findAllByCustomer(customerId).stream()
+            .map { credit: Credit -> CreditViewList(credit) }
+            .collect(Collectors.toList())
+    }
+
+    @GetMapping
+    fun findByCreditCode(
+        @RequestParam(value = "customerId") customerId: Long,
+        @PathVariable creditCode: UUID
+    ): CreditView {
+        val credit: Credit = this.creditService.findByCreditCode(customerId, creditCode)
+        return CreditView(credit)
     }
 }
