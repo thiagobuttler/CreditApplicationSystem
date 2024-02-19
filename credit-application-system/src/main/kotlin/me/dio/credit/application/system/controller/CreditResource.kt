30c7032497152b36c1f5fb8b33438bae692251b6
Thiago <thiagobuttler@outlook.com>
2024-02-18 22:03:35 -0300

    Funcao findAllByCustomer e classe CreditViewList

--- credit-application-system/src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
@@ -1,13 +1,18 @@
 package me.dio.credit.application.system.controller
 
 import me.dio.credit.application.system.dto.CreditDto
+import me.dio.credit.application.system.dto.CreditViewList
 import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.service.impl.CreditService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Collectors
 
+@Suppress("UNREACHABLE_CODE")
 @RestController
 @RequestMapping("/api/credits")
 class CreditResource(
@@ -18,5 +23,12 @@ class CreditResource(
     fun saveCredit(@RequestBody creditDto: CreditDto): String {
         val credit: Credit = this.creditService.save(creditDto.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!"
+
+        @GetMapping
+        fun findAllByCustomerId(@RequestParam(value = "customerId") customerId: Long): List<CreditViewList> {
+            return this.creditService.findAllByCustomer(customerId).stream()
+                .map { credit: Credit -> CreditViewList(credit) }
+                .collect(Collectors.toList())
+        }
     }
 }
