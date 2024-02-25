f41cdaf087d36abec48be1a6028a63c8966e7e71
Thiago <thiagobuttler@outlook.com>
2024-02-25 20:22:35 -0300

    CustomerRepositoryTest

---
+++ credit-application-system/src/test/kotlin/me/dio/credit/application/system/repository/CreditRepositoryTest.kt
@@ -0,0 +1,17 @@
+package me.dio.credit.application.system.repository
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
+import org.springframework.test.context.ActiveProfiles
+
+@ActiveProfiles("test")
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CreditRepositoryTest {
+    @Autowired lateinit var customerRepository: CustomerRepository
+    @Autowired lateinit var testEntityManager: TestEntityManager
+
+
+}
