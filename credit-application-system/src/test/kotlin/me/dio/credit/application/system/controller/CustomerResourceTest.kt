a6aca823f2a98841fcbf08f329605f5f1002c1bc
Thiago <thiagobuttler@outlook.com>
2024-02-25 21:53:26 -0300

    test CustomerResource - save, POST and 201 return

--- credit-application-system/src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
+++ credit-application-system/src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
@@ -1,15 +1,22 @@
 package me.dio.credit.application.system.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import me.dio.credit.application.system.dto.CustomerDto
 import me.dio.credit.application.system.repository.CustomerRepository
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -26,4 +33,39 @@ class CustomerResourceTest {
 
     @BeforeEach fun setup() = customerRepository.deleteAll()
     @AfterEach fun tearDown() = customerRepository.deleteAll()
+
+    @Test
+    fun `should create a customer and return 201 status`() {
+        //given
+        val customerDto: CustomerDto = builderCustomerDto()
+        val valueAsString: String = objetcMapper.writeValueAsString(customerDto)
+        //when
+        //then
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isCreated)
+            .andExpect(MockMvcResultMatchers.jsonPath("$.firstName").value("Taylor"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.street").value("Cornelia Street, 13"))
+            .andDo(MockMvcResultHandlers.print())
+    }
+
+    private fun builderCustomerDto(
+        firstName: String = "Taylor",
+        lastName: String = "Swift",
+        cpf: String = "914.491.250-18",
+        email: String = "taylorswift@outlook.com",
+        income: BigDecimal = BigDecimal.valueOf(1100000000.0),
+        password: String = "131313",
+        zipCode: String = "131313",
+        street: String = "Cornelia Street, 13"
+    ) = CustomerDto (
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        income = income,
+        password = password,
+        zipCode = zipCode,
+        street = street
+    )
 }
