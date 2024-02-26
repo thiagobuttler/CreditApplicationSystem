8d7150318ac9228b588a07d9069825dce1087141
Thiago <thiagobuttler@outlook.com>
2024-02-25 22:12:24 -0300

    test CustomerResource - save, error 409 for same CPF

--- credit-application-system/src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
+++ credit-application-system/src/test/kotlin/me/dio/credit/application/system/controller/CustomerResourceTest.kt
@@ -49,6 +49,30 @@ class CustomerResourceTest {
             .andDo(MockMvcResultHandlers.print())
     }
 
+    @Test
+    fun `should not save a customer with same CPF and return 409 status`() {
+        //given
+        customerRepository.save(builderCustomerDto().toEntity())
+        val customerDto: CustomerDto = builderCustomerDto()
+        val valueAsString: String = objetcMapper.writeValueAsString(customerDto)
+        //when
+        mockMvc.perform(MockMvcRequestBuilders.post(URL)
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isConflict)
+            .andDo(MockMvcResultHandlers.print())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.title").value("Conflict! Consult the documentation"))
+            .andExpect(MockMvcResultMatchers.jsonPath("$.timestamp").exists())
+            .andExpect(MockMvcResultMatchers.jsonPath("$.status").value(409))
+            .andExpect(
+                MockMvcResultMatchers.jsonPath("$.exception")
+                    .value("class org.springframework.dao.DataIntegrityViolationException")
+            )
+            .andExpect(MockMvcResultMatchers.jsonPath("$.details[*]").isNotEmpty)
+            .andDo(MockMvcResultHandlers.print())
+        //then
+    }
+
     private fun builderCustomerDto(
         firstName: String = "Taylor",
         lastName: String = "Swift",
