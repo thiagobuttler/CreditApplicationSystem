23767ff09226c3be45401fc9ea9b8afcfe4c53bd
Thiago <thiagobuttler@outlook.com>
2024-03-05 22:43:57 -0300

    Should create a credit and return 201 status implementado

--- credit-application-system/src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
+++ credit-application-system/src/main/kotlin/me/dio/credit/application/system/controller/CreditResource.kt
@@ -8,14 +8,8 @@ import me.dio.credit.application.system.entity.Credit
 import me.dio.credit.application.system.service.impl.CreditService
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import java.util.*
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 import java.util.stream.Collectors
 
 @Suppress("UNREACHABLE_CODE")
@@ -28,8 +22,7 @@ class CreditResource(
     @PostMapping
     fun saveCredit(@RequestBody @Valid creditDto: CreditDto): ResponseEntity<String> {
         val credit: Credit = this.creditService.save(creditDto.toEntity())
-        return ResponseEntity.status(HttpStatus.CREATED)
-            .body("Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!")
+        return ResponseEntity.status(HttpStatus.CREATED).body("Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved!")
     }
 
     @GetMapping
--- credit-application-system/src/test/kotlin/me/dio/credit/application/system/controller/CreditResourceTest.kt
+++ credit-application-system/src/test/kotlin/me/dio/credit/application/system/controller/CreditResourceTest.kt
@@ -1,35 +1,88 @@
-/*package me.dio.credit.application.system.controller
+package me.dio.credit.application.system.controller
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import me.dio.credit.application.system.dto.CreditDto
+import me.dio.credit.application.system.dto.CustomerDto
 import me.dio.credit.application.system.repository.CreditRepository
+import me.dio.credit.application.system.repository.CustomerRepository
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.http.MediaType
 import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.ContextConfiguration
 import org.springframework.test.web.servlet.MockMvc
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.math.BigDecimal
+import java.time.LocalDate
 
 @SpringBootTest
 @ActiveProfiles("test")
 @AutoConfigureMockMvc
 @ContextConfiguration
 class CreditResourceTest {
-    private lateinit var creditRepository: CreditRepository
-    private lateinit var mockMvc: MockMvc
-    private lateinit var objectMapper: ObjectMapper
+    @Autowired private lateinit var creditRepository: CreditRepository
+    @Autowired private lateinit var customerRepository: CustomerRepository
+    @Autowired private lateinit var mockMvc: MockMvc
+    @Autowired private lateinit var objectMapper: ObjectMapper
 
     companion object {
         const val URL = "/api/credits"
     }
 
-    @BeforeEach fun setup() = creditRepository.deleteAll()
-    @AfterEach fun tearDown() = creditRepository.deleteAll()
+    @BeforeEach fun setup() {
+        creditRepository.deleteAll()
+        builderCustomerDto()?.let { customerRepository.save(it.toEntity()) }
+    }
+    @AfterEach fun tearDown() {
+        creditRepository.deleteAll()
+        customerRepository.deleteAll()
+    }
+
 
     @Test
     fun `should create a credit and return 201 status`() {
-
+        // given
+        val creditDto: CreditDto = builderCreditDto()
+        val valueAsString: String = objectMapper.writeValueAsString(creditDto)
+        // when
+        // then
+        mockMvc.perform(MockMvcRequestBuilders.post(URL).contentType(MediaType.APPLICATION_JSON).content(valueAsString))
+            .andExpect(MockMvcResultMatchers.status().isCreated)
     }
+
+    private fun builderCreditDto(
+        creditValue: BigDecimal = BigDecimal("10000.0"),
+        dayFirstOfInstallment: LocalDate = LocalDate.of(2024, 3, 10),
+        numberOfInstallments: Int = 2,
+        customerId: Long = 1
+    ) = CreditDto (
+        creditvalue = creditValue,
+        dayFirstOfInstallment = dayFirstOfInstallment,
+        numberOfInstallments = numberOfInstallments,
+        customerId = customerId
+    )
+    private fun builderCustomerDto(
+        firstName: String = "Taylor",
+        lastName: String = "Swift",
+        cpf: String = "914.491.250-18",
+        email: String = "taylorswift@outlook.com",
+        income: BigDecimal = BigDecimal.valueOf(1100000000.0),
+        password: String = "131313",
+        zipCode: String = "131313",
+        street: String = "Cornelia Street, 13"
+    ) = CustomerDto (
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        income = income,
+        password = password,
+        zipCode = zipCode,
+        street = street
+    )
 }
-*/
